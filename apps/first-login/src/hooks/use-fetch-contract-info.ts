0126a86ef991b2d613138ad55091a5ebf0650b51
hoanmini <>
2026-04-21 09:53:21 +0700

    refactor: ♻️ migrate to React 19 use hook, update CSS variables for dark mode, and improve type safety

--- apps/first-login/src/hooks/use-fetch-contract-info.ts
+++ apps/first-login/src/hooks/use-fetch-contract-info.ts
@@ -3,8 +3,7 @@
 import { useQuery, type UseQueryOptions } from '@tanstack/react-query'
 import { QUERY_KEY } from '../lib/react-query'
 
-const urlContractInfo =
-  import.meta.env.VITE_URI_CONTRACT_INFO || 'http://192.168.1.242:5500/contract-info.json'
+const urlContractInfo = (import.meta.env.VITE_URI_CONTRACT_INFO as string) || ''
 
 export interface ContractInfo {
   port: number
@@ -24,13 +23,13 @@ export function createFetchContractInfoQueryOptions(): UseQueryOptions<
       try {
         const response = await fetch(`${urlContractInfo}?v=${Date.now()}`)
         if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`)
-        const contractInfo: ContractInfo = await response.json()
+        const contractInfo = (await response.json()) as ContractInfo
         return contractInfo
       } catch (error) {
         console.error(error)
         throw error
       }
-    }
+    },
   }
 }
 
