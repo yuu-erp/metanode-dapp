e7ac7f6deba0665bced6061a59bc32d0d529af94
hoanmini <>
2026-06-09 10:50:14 +0700

    refactor: remove the entire setting application from the project workspace

--- apps/setting/src/features/setting-profile/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as SettingProfile } from './setting-profile'
-export { default as GeneralSetting } from './general-setting'
-export { default as SecureSetting } from './secure-setting'
